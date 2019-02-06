78f1e5658063ed58ef3de9368527bdc9874e796f
Ronan Connolly <ronanc314@gmail.com>
2019-02-06 17:06:19 +0000

    Update PWA data

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,18 +31,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Ronan Connolly`,
+        short_name: `RC`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#524763`,
+        theme_color: `#524763`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    'gatsby-plugin-offline',
+    'gatsby-plugin-offline', // this should be after the manifest file.
     {
       resolve: `gatsby-source-filesystem`,
       options: {
