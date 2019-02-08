787bc6eec8ce355d850cb52d9f30bfda5a2f093c
Ronan Connolly <ronanc314@gmail.com>
2019-02-08 20:44:01 +0000

    Move contact form into main page rather than component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -38,8 +38,37 @@ const ContactPage = ({ location }) => (
             kogi ea salvia DIY lorem.
           </p>
 
-          <ContactForm />
+          {/* <ContactForm /> */}
           {/* PUT FORM DIRECTLY IN HERE< NOT AS A COMMPONENT */}
+          <form name="contact" method="POST" data-netlify="true">
+            <p>
+              <label>
+                Your Name: <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Email: <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Role:{' '}
+                <select name="role[]" multiple>
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select>
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message" />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
         </div>
       </Layout>
     )}
