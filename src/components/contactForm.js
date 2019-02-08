c323ecaec3bdeac8b0eb205e0a673fa03de025a6
Ronan Connolly <ronanc314@gmail.com>
2019-02-08 21:23:26 +0000

    Update contact form

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,4 +1,153 @@
+// import React from 'react'
+// import styled from 'styled-components'
+
+// const Form = styled.form`
+//   /* border: 1px solid black; */
+//   margin: auto;
+//   /* margin-top: 100px; */
+//   /* background: yellow; */
+
+//   width: 602px;
+//   /* text-align: center; */
+
+//   /* position: absolute;
+//   top: 50%;
+//   height: 400px;*/
+//   margin-top: 100px;
+
+//   label {
+//     width: 100px;
+//     display: inline-block;
+//   }
+
+//   input {
+//     /* margin-left: 10px; */
+//     width: 500px;
+//   }
+
+//   textarea {
+//     width: 600px;
+//   }
+
+//   .hidden {
+//     visibility: hidden;
+//     position: absolute;
+//   }
+
+//   .actions {
+//     width: 600px;
+//     margin: 0;
+//     text-align: center;
+//     li {
+//       display: inline;
+//       button,
+//       input {
+//         width: 200px;
+//         margin-left: 20px;
+//         margin-right: 20px;
+//       }
+//     }
+//   }
+// `
+
+// const ContactForm = () => (
+//   <form name="contact" method="POST" data-netlify="true">
+//     <p>
+//       <label>
+//         Your Name: <input type="text" name="name" />
+//       </label>
+//     </p>
+//     <p>
+//       <label>
+//         Your Email: <input type="email" name="email" />
+//       </label>
+//     </p>
+//     <p>
+//       <label>
+//         Your Role:{' '}
+//         <select name="role[]" multiple>
+//           <option value="leader">Leader</option>
+//           <option value="follower">Follower</option>
+//         </select>
+//       </label>
+//     </p>
+//     <p>
+//       <label>
+//         Message: <textarea name="message" />
+//       </label>
+//     </p>
+//     <p>
+//       <button type="submit">Send</button>
+//     </p>
+//   </form>
+
+//   // <form
+//   //   name="contact"
+//   //   method="post"
+//   //   data-netlify="true"
+//   //   data-netlify-honeypot="bot-field"
+//   // >
+//   //   <input type="hidden" name="bot-field" />
+//   //   <div className="field half first">
+//   //     <label htmlFor="name">Name</label>
+//   //     <input type="text" name="name" id="name" />
+//   //   </div>
+//   //   <div className="field half">
+//   //     <label htmlFor="email">Email</label>
+//   //     <input type="text" name="email" id="email" />
+//   //   </div>
+//   //   <div className="field">
+//   //     <label htmlFor="message">Message</label>
+//   //     <textarea name="message" id="message" rows="6" />
+//   //   </div>
+//   //   <ul className="actions">
+//   //     <li>
+//   //       <input type="submit" value="Send Message" className="special" />
+//   //     </li>
+//   //     <li>
+//   //       <input type="reset" value="Clear" />
+//   //     </li>
+//   //   </ul>
+//   // </form>
+
+//   // <Form
+//   //   name="contact"
+//   //   method="POST"
+//   //   netlify-honeypot="bot-field"
+//   //   data-netlify="true"
+//   // >
+//   //   <p className="hidden">
+//   //     <label>
+//   //       Don’t fill this out if you're human: <input name="bot-field" />
+//   //     </label>
+//   //   </p>
+//   //   <p>
+//   //     <label>Name:</label>
+//   //     <input type="text" name="name" />
+//   //   </p>
+//   //   <p>
+//   //     <label>Email:</label>
+//   //     <input type="email" name="email" />
+//   //   </p>
+//   //   <label>Message:</label>
+//   //   <p>
+//   //     <textarea name="message" rows="6" />
+//   //   </p>
+//   //   <ul className="actions">
+//   //     <li>
+//   //       <button type="submit">Send</button>
+//   //     </li>
+//   //     <li>
+//   //       <input type="reset" value="Clear" />
+//   //     </li>
+//   //   </ul>
+//   // </Form>
+// )
+
+// export default ContactForm
+
 import React from 'react'
+import { navigateTo } from 'gatsby-link'
 import styled from 'styled-components'
 
 const Form = styled.form`
@@ -14,6 +163,7 @@ const Form = styled.form`
   top: 50%;
   height: 400px;*/
   margin-top: 100px;
+  margin-bottom: 100px;
 
   label {
     width: 100px;
@@ -49,99 +199,131 @@ const Form = styled.form`
     }
   }
 `
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
 
-const ContactForm = () => (
-  <form name="contact" method="POST" data-netlify="true">
-    <p>
-      <label>
-        Your Name: <input type="text" name="name" />
-      </label>
-    </p>
-    <p>
-      <label>
-        Your Email: <input type="email" name="email" />
-      </label>
-    </p>
-    <p>
-      <label>
-        Your Role:{' '}
-        <select name="role[]" multiple>
-          <option value="leader">Leader</option>
-          <option value="follower">Follower</option>
-        </select>
-      </label>
-    </p>
-    <p>
-      <label>
-        Message: <textarea name="message" />
-      </label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
-  </form>
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
 
-  // <form
-  //   name="contact"
-  //   method="post"
-  //   data-netlify="true"
-  //   data-netlify-honeypot="bot-field"
-  // >
-  //   <input type="hidden" name="bot-field" />
-  //   <div className="field half first">
-  //     <label htmlFor="name">Name</label>
-  //     <input type="text" name="name" id="name" />
-  //   </div>
-  //   <div className="field half">
-  //     <label htmlFor="email">Email</label>
-  //     <input type="text" name="email" id="email" />
-  //   </div>
-  //   <div className="field">
-  //     <label htmlFor="message">Message</label>
-  //     <textarea name="message" id="message" rows="6" />
-  //   </div>
-  //   <ul className="actions">
-  //     <li>
-  //       <input type="submit" value="Send Message" className="special" />
-  //     </li>
-  //     <li>
-  //       <input type="reset" value="Clear" />
-  //     </li>
-  //   </ul>
-  // </form>
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
 
-  // <Form
-  //   name="contact"
-  //   method="POST"
-  //   netlify-honeypot="bot-field"
-  //   data-netlify="true"
-  // >
-  //   <p className="hidden">
-  //     <label>
-  //       Don’t fill this out if you're human: <input name="bot-field" />
-  //     </label>
-  //   </p>
-  //   <p>
-  //     <label>Name:</label>
-  //     <input type="text" name="name" />
-  //   </p>
-  //   <p>
-  //     <label>Email:</label>
-  //     <input type="email" name="email" />
-  //   </p>
-  //   <label>Message:</label>
-  //   <p>
-  //     <textarea name="message" rows="6" />
-  //   </p>
-  //   <ul className="actions">
-  //     <li>
-  //       <button type="submit">Send</button>
-  //     </li>
-  //     <li>
-  //       <input type="reset" value="Clear" />
-  //     </li>
-  //   </ul>
-  // </Form>
-)
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+      .then(() => navigateTo(form.getAttribute('action')))
+      .catch(error => alert(error))
+  }
 
-export default ContactForm
+  render() {
+    return (
+      <div>
+        {/* <Form
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out:{' '}
+              <input name="bot-field" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your name:
+              <br />
+              <input type="text" name="name" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:
+              <br />
+              <input type="email" name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:
+              <br />
+              <textarea name="message" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </Form> */}
+
+        <Form
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          <p className="hidden">
+            <label>
+              Don’t fill this out if you're human: <input name="bot-field" />
+            </label>
+          </p>
+          <p>
+            <label>Name:</label>
+            <input
+              type="text"
+              name="name"
+              onChange={this.handleChange}
+              required
+            />
+          </p>
+          <p>
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              onChange={this.handleChange}
+              required
+            />
+          </p>
+          <label>Message:</label>
+          <p>
+            <textarea
+              name="message"
+              rows="6"
+              onChange={this.handleChange}
+              required
+            />
+          </p>
+          <ul className="actions">
+            <li>
+              <button type="submit">Send</button>
+            </li>
+            <li>
+              <input type="reset" value="Clear" />
+            </li>
+          </ul>
+        </Form>
+      </div>
+    )
+  }
+}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,169 +1,169 @@
-// import React from 'react'
-// import Img from 'gatsby-image'
-// import { StaticQuery, graphql } from 'gatsby'
-
-// import Layout from '../components/layout'
-// import ContactForm from '../components/contactForm'
-
-// const ContactPage = ({ location }) => (
-//   <StaticQuery
-//     query={graphql`
-//       query ContactImageQuery {
-//         file(relativePath: { regex: "/typewriter/" }) {
-//           childImageSharp {
-//             fluid(maxWidth: 2000) {
-//               ...GatsbyImageSharpFluid_tracedSVG
-//             }
-//           }
-//         }
-//       }
-//     `}
-//     render={data => (
-//       <Layout location={location}>
-//         <div>
-//           <Img
-//             fluid={data.file.childImageSharp.fluid}
-//             style={{
-//               width: '500px',
-//               margin: 'auto',
-//             }}
-//           />
-//           <p>
-//             Lorem ipsum dolor amet pitchfork chicharrones brunch ugh twee
-//             microdosing intelligentsia kogi semiotics meh. Disrupt meditation
-//             letterpress next level marfa laboris. Godard messenger bag next
-//             level, mumblecore art party copper mug anim craft beer magna meh
-//             narwhal deserunt biodiesel glossier. Man braid scenester heirloom
-//             green juice occaecat culpa. Ex chicharrones deserunt master cleanse
-//             kogi ea salvia DIY lorem.
-//           </p>
-
-//           {/* // TODO: TRY this next: https://github.com/imorente/gatsby-netlify-form-example/blob/master/src/pages/contact.js */}
-
-//           {/* <ContactForm /> */}
-//           {/* PUT FORM DIRECTLY IN HERE< NOT AS A COMMPONENT */}
-//           <form
-//             name="contact"
-//             method="post"
-//             data-netlify="true"
-//             actoin="/pages/success"
-//           >
-//             <p>
-//               <label>
-//                 Your Name: <input type="text" name="name" />
-//               </label>
-//             </p>
-//             <p>
-//               <label>
-//                 Your Email: <input type="email" name="email" />
-//               </label>
-//             </p>
-//             <p>
-//               <label>
-//                 Your Role:{' '}
-//                 <select name="role[]" multiple>
-//                   <option value="leader">Leader</option>
-//                   <option value="follower">Follower</option>
-//                 </select>
-//               </label>
-//             </p>
-//             <p>
-//               <label>
-//                 Message: <textarea name="message" />
-//               </label>
-//             </p>
-//             <p>
-//               <button type="submit">Send</button>
-//             </p>
-//           </form>
-//         </div>
-//       </Layout>
-//     )}
-//   />
-// )
-
-// export default ContactPage
-
 import React from 'react'
-import { navigateTo } from 'gatsby-link'
+import Img from 'gatsby-image'
+import { StaticQuery, graphql } from 'gatsby'
 
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
-}
+import Layout from '../components/layout'
+import ContactForm from '../components/contactForm'
 
-export default class Contact extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
-
-  handleSubmit = e => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...this.state,
-      }),
-    })
-      .then(() => navigateTo(form.getAttribute('action')))
-      .catch(error => alert(error))
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Contact</h1>
-        <form
-          name="contact"
-          method="post"
-          action="/thanks/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit={this.handleSubmit}
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out:{' '}
-              <input name="bot-field" onChange={this.handleChange} />
-            </label>
-          </p>
+const ContactPage = ({ location }) => (
+  <StaticQuery
+    query={graphql`
+      query ContactImageQuery {
+        file(relativePath: { regex: "/typewriter/" }) {
+          childImageSharp {
+            fluid(maxWidth: 2000) {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Layout location={location}>
+        <div>
+          <Img
+            fluid={data.file.childImageSharp.fluid}
+            style={{
+              width: '500px',
+              margin: 'auto',
+            }}
+          />
           <p>
-            <label>
-              Your name:
-              <br />
-              <input type="text" name="name" onChange={this.handleChange} />
-            </label>
+            Lorem ipsum dolor amet pitchfork chicharrones brunch ugh twee
+            microdosing intelligentsia kogi semiotics meh. Disrupt meditation
+            letterpress next level marfa laboris. Godard messenger bag next
+            level, mumblecore art party copper mug anim craft beer magna meh
+            narwhal deserunt biodiesel glossier. Man braid scenester heirloom
+            green juice occaecat culpa. Ex chicharrones deserunt master cleanse
+            kogi ea salvia DIY lorem.
           </p>
-          <p>
-            <label>
-              Your email:
-              <br />
-              <input type="email" name="email" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message:
-              <br />
-              <textarea name="message" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-      </div>
-    )
-  }
-}
+
+          {/* // TODO: TRY this next: https://github.com/imorente/gatsby-netlify-form-example/blob/master/src/pages/contact.js */}
+
+          <ContactForm />
+          {/* PUT FORM DIRECTLY IN HERE< NOT AS A COMMPONENT */}
+          {/* <form
+            name="contact"
+            method="post"
+            data-netlify="true"
+            actoin="/pages/success"
+          >
+            <p>
+              <label>
+                Your Name: <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Email: <input type="email" name="email" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your Role:{' '}
+                <select name="role[]" multiple>
+                  <option value="leader">Leader</option>
+                  <option value="follower">Follower</option>
+                </select>
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message" />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form> */}
+        </div>
+      </Layout>
+    )}
+  />
+)
+
+export default ContactPage
+
+// import React from 'react'
+// import { navigateTo } from 'gatsby-link'
+
+// function encode(data) {
+//   return Object.keys(data)
+//     .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+//     .join('&')
+// }
+
+// export default class Contact extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {}
+//   }
+
+//   handleChange = e => {
+//     this.setState({ [e.target.name]: e.target.value })
+//   }
+
+//   handleSubmit = e => {
+//     e.preventDefault()
+//     const form = e.target
+//     fetch('/', {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+//       body: encode({
+//         'form-name': form.getAttribute('name'),
+//         ...this.state,
+//       }),
+//     })
+//       .then(() => navigateTo(form.getAttribute('action')))
+//       .catch(error => alert(error))
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Contact</h1>
+//         <form
+//           name="contact"
+//           method="post"
+//           action="/thanks/"
+//           data-netlify="true"
+//           data-netlify-honeypot="bot-field"
+//           onSubmit={this.handleSubmit}
+//         >
+//           {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+//           <input type="hidden" name="form-name" value="contact" />
+//           <p hidden>
+//             <label>
+//               Don’t fill this out:{' '}
+//               <input name="bot-field" onChange={this.handleChange} />
+//             </label>
+//           </p>
+//           <p>
+//             <label>
+//               Your name:
+//               <br />
+//               <input type="text" name="name" onChange={this.handleChange} />
+//             </label>
+//           </p>
+//           <p>
+//             <label>
+//               Your email:
+//               <br />
+//               <input type="email" name="email" onChange={this.handleChange} />
+//             </label>
+//           </p>
+//           <p>
+//             <label>
+//               Message:
+//               <br />
+//               <textarea name="message" onChange={this.handleChange} />
+//             </label>
+//           </p>
+//           <p>
+//             <button type="submit">Send</button>
+//           </p>
+//         </form>
+//       </div>
+//     )
+//   }
+// }
