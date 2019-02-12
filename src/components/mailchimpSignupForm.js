0d1f64b19acb5a7bdd1fdecb2be082546729e838
Ronan Connolly <ronanc314@gmail.com>
2019-02-12 21:23:48 +0000

    Create MailchimpSignupForm component

---
+++ src/components/mailchimpSignupForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const FormWrapper = styled.section`
+  form {
+  }
+`
+
+const MailchimpSignupForm = props => (
+  <FormWrapper>
+    <form action="post" />
+  </FormWrapper>
+)
+
+export default MailchimpSignupForm
