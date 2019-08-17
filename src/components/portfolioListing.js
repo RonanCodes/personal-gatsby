aedaa7df5f74777f5ff70eaa6f3081c8ee9f0d6f
Ronan Connolly <ronanc314@gmail.com>
2019-08-17 17:19:19 +0100

    Update formatString format

--- src/components/portfolioListing.js
+++ src/components/portfolioListing.js
@@ -22,8 +22,8 @@ const PORTFOLIO_LISTING_QUERY = graphql`
           frontmatter {
             title
             coverImage
-            startDate(formatString: "MMM YYYY")
-            endDate(formatString: "MMM YYYY")
+            startDate(formatString: "MMM DD, YYYY")
+            endDate(formatString: "MMM DD, YYYY")
             technologiesandskills
             goal
             whatidid
